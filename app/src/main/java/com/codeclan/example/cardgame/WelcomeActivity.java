2b452f1b01a09a64c1381320981d662e6ea17331
Farhan Sadik <mdfarhansadik2002@gmail.com>
2023-09-23 21:35:31 +0600

    hellow this is the first push

--- app/src/main/java/com/codeclan/example/cardgame/WelcomeActivity.java
+++ app/src/main/java/com/codeclan/example/cardgame/WelcomeActivity.java
@@ -6,9 +6,11 @@ package com.codeclan.example.cardgame;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -18,6 +20,7 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
     }
 
     public void onUserEnterButtonClick(View view) {
